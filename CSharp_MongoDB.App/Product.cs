5122938bd588f6c1d33df9dbdb598f841f446bb0
Starinin Andrey <starininandrey@gmail.com>
2022-09-14 20:34:30 +0300

    Добавление десериализации

---
+++ CSharp_MongoDB.App/Product.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CSharp_MongoDB.App;
+
+public class Info
+{
+    [BsonElement("measure")]
+    public string? Measure { get; set; }
+
+    [BsonElement("amount")]
+    public double? Amount { get; set; }
+}
+
+public class Product
+{
+    public ObjectId Id { get; set; }
+
+    [BsonElement("product_name")]
+    public string? Name { get; set; }
+
+    [BsonElement("price")]
+    public double? Price { get; set; }
+
+    [BsonElement("info")]
+    public Info? Info { get; set; }
+}
--- CSharp_MongoDB.App/Program.cs
+++ CSharp_MongoDB.App/Program.cs
@@ -1,4 +1,6 @@
-﻿using MongoDB.Bson;
+﻿using CSharp_MongoDB.App;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 
 const string CONNECTION_STRING = "mongodb://localhost:27017";
@@ -6,26 +8,9 @@ var client = new MongoClient(CONNECTION_STRING);
 
 var db = client.GetDatabase("products_db");
 var collection = db.GetCollection<BsonDocument>("products");
-var cursor = collection.FindSync(new BsonDocument());
-while (cursor.MoveNext())
+var products = collection.FindSync(new BsonDocument()).ToList();
+foreach (var document in products)
 {
-    foreach (var document in cursor.Current)
-    {
-        foreach (var element in document.Elements)
-        {
-            Console.Write($"{element.Name} -> ");
-            if (element.Value.IsBsonDocument)
-            {
-                foreach (var item in element.Value.AsBsonDocument)
-                {
-                    Console.WriteLine($"{item.Name} -> {item.Value}");
-                }
-            }
-            else
-            {
-                Console.WriteLine($"{element.Value}");
-            }
-        }
-        Console.WriteLine();
-    }
+    var product = BsonSerializer.Deserialize<Product>(document);
+    Console.WriteLine($"{product.Name}: {product.Info?.Amount}");
 }
