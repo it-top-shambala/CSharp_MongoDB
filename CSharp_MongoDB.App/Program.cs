105e8e24366679c18b425087df498f4c7bf8ff61
Starinin Andrey <starininandrey@gmail.com>
2022-09-14 19:14:47 +0300

    Вывод информации обо всех БД

---
+++ CSharp_MongoDB.App/Program.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+
+const string CONNECTION_STRING = "mongodb://localhost:27017";
+var db = new MongoClient(CONNECTION_STRING);
+var bases = db.ListDatabases();
+while (bases.MoveNext())
+{
+    foreach (var item in bases.Current)
+    {
+        foreach (var element in item.Elements)
+        {
+            Console.WriteLine($"{element.Name} -> {element.Value}");
+        }
+        Console.WriteLine("--- --- ---");
+    }
+}
+
