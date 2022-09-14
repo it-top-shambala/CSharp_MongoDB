090f3e042c7ed1c5aeb0e2ff308beb316758872b
Starinin Andrey <starininandrey@gmail.com>
2022-09-14 19:51:11 +0300

    Вывод документов коллекции

--- CSharp_MongoDB.App/Program.cs
+++ CSharp_MongoDB.App/Program.cs
@@ -1,17 +1,30 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 
 const string CONNECTION_STRING = "mongodb://localhost:27017";
 var client = new MongoClient(CONNECTION_STRING);
 
-var db = client.GetDatabase("students_db");
-var collections = db.ListCollections();
-while (collections.MoveNext())
+var db = client.GetDatabase("products_db");
+var collection = db.GetCollection<BsonDocument>("products");
+var cursor = collection.FindSync(new BsonDocument());
+while (cursor.MoveNext())
 {
-    foreach (var document in collections.Current)
+    foreach (var document in cursor.Current)
     {
         foreach (var element in document.Elements)
         {
-            Console.WriteLine($"{element.Name} -> {element.Value}");
+            Console.Write($"{element.Name} -> ");
+            if (element.Value.IsBsonDocument)
+            {
+                foreach (var item in element.Value.AsBsonDocument)
+                {
+                    Console.WriteLine($"{item.Name} -> {item.Value}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{element.Value}");
+            }
         }
         Console.WriteLine();
     }
